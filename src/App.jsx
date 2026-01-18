e0bc2254114e4baa546a3dce22059e6ceb816975
Miaritsoa2424 <tsoamiari24@gmail.com>
2026-01-18 17:53:01 +0300

    initialize React project with Vite setup and basic configuration

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [apiMsg, setApiMsg] = useState('Chargement...')
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      try {
+        const res = await fetch('http://localhost:8080/test')
+        if (!res.ok) throw new Error(await res.text())
+        const text = await res.text()
+        setApiMsg(text)
+      } catch (e) {
+        setApiMsg('Erreur: ' + (e.message ?? e))
+      }
+    }
+    fetchApi()
+  }, [])
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <p style={{ marginTop: '1rem' }}>Backend API: {apiMsg}</p>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
